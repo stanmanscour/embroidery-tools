a89cef827e37355754b2f1803a0a49faa5c8cec8
Stanislas <stan@MacBook-de-Stan.local>
2024-03-05 10:32:45 +0100

    add images and remove scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`overscroll-none overflow-hidden ${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
